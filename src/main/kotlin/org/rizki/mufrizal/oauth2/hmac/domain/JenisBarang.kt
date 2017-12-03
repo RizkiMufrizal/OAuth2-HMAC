ead742f5fa9182b3f48a6cfddbab7bb5e8ed9e6d
rizkimufrizal <mufrizalrizki@gmail.com>
2017-12-03 19:01:22 +0700

    add enum jenis barang

---
+++ src/main/kotlin/org/rizki/mufrizal/oauth2/hmac/domain/JenisBarang.kt
@@ -0,0 +1,17 @@
+package org.rizki.mufrizal.oauth2.hmac.domain
+
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Web <https://RizkiMufrizal.github.io>
+ * @Since 03 December 2017
+ * @Time 14.22
+ * @Project oauth2-HMAC
+ * @Package org.rizki.mufrizal.oauth2.hmac.domain
+ * @File JenisBarang
+ *
+ */
+enum class JenisBarang {
+    cair, padat, gas
+}
